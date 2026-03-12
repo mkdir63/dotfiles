c3cb61081ac52852de1e84ec21ed01c1d23ab132
Amedeo Carbone <bjx765@ku.dk>
2026-03-12 17:16:45 +0100

    Adding powershell dotfiles

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Set-Location $HOME\Documents
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config "C:\Users\bjx765\OneDrive - University of Copenhagen\Documents\PowerShell\uew.omp.json" | Invoke-Expression
